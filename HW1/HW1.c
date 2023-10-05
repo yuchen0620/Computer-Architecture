3ed480ecb7509dd44484e6d8b595a220f0c645c8
Yuchen0620 <chouyuchen0620@gmail.com>
2023-10-05 14:35:45 +0800

    HW1

--- HW1/HW1.c
+++ HW1/HW1.c
@@ -7,7 +7,6 @@ uint16_t count_leading_zeros(uint32_t x)
     x |= (x >> 4);
     x |= (x >> 8);
     x |= (x >> 16);
-    
 
     /* count ones (population count) */
     x -= ((x >> 1) & 0x55555555  );
@@ -16,12 +15,20 @@ uint16_t count_leading_zeros(uint32_t x)
     x += (x >> 8);
     x += (x >> 16);
    
-
     return (32 - (x & 0x3f));
 }
 
+uint32_t generate_bitmask(uint32_t max_value)
+{
+    uint16_t leading_zeros = count_leading_zeros(max_value);
+    return (1 << (32 - leading_zeros)) - 1;
+}
 int main()
 {   
-    int y = count_leading_zeros(0);
-    printf("%d",y);
+    uint32_t test_data[] = {0, 4, 0x40000000};
+    for (int i = 0; i < 3; i++){
+        printf("The reslut of %d is %d\n", test_data[i],generate_bitmask(test_data[i]));
+    }
 }
+
+
